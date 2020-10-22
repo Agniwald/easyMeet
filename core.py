6f1ff059c2ef0313f8ef9de875fb2b8de57bf4d1
DSH01 <dmitriy.shilkin.001@gmail.com>
2020-10-22 14:13:56 +0300

    google login test #6

--- core.py
+++ core.py
@@ -67,12 +67,12 @@ def send_captcha(text):
 
 
 def send_password():
-	driver.save_screenshot("static/img/3.png")
+	driver.save_screenshot("static/img/4.png")
 	print(driver.page_source)
 	logging.info(driver.page_source)
 
-	WebDriverWait(driver, 20).until(EC.invisibility_of_element((By.NAME, 'Passwd')))
-	driver.save_screenshot("static/img/4.png")
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, 'Passwd')))
+	driver.save_screenshot("static/img/5.png")
 	driver.find_element_by_name('Passwd').send_keys(PASSWORD)
 
 	driver.find_element_by_id('submit').click()
